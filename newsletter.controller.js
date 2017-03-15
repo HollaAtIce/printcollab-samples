77c8f71f0782e29201a2e0ac30c32babd23404be
HollaAtIce <isaac.cuchilla@gmail.com>
2017-03-14 22:56:04 -0700

    Add files via upload

---
+++ newsletter.controller.js
@@ -0,0 +1,21 @@
+const responses = require('../models/responses')
+const newsletterService = require('../services/newsletter.service')()
+
+module.exports = newsletterController
+
+function newsletterController() {
+    return {
+        insertEmail
+    }
+
+    function insertEmail(req, res) {
+        newsletterService.insertEmail(req.body)
+            .then((member) => {
+                const responseModel = new responses.ItemResponse()
+                responseModel.item = member
+                res.json(responseModel)
+            }).catch((err) => {
+                res.status(500).send((err))
+            })
+    }
+}
