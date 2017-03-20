a0fc4811d708c7fd83eae5ddfccba232162ee83b
Isaac.Cuchilla <isaac.cuchilla@gmail.com>
2017-03-19 19:10:17 -0700

    Add files via upload

---
+++ newsletter.service.js
@@ -0,0 +1,14 @@
+ module.exports = newsletterService
+ const mailchimp = require('mailchimp-v3')
+ const mailchimpList = process.env.MAILCHIMP_LIST
+ mailchimp.setApiKey(process.env.MAILCHIMP_API_KEY)
+
+ function newsletterService() {
+     return {
+         insertEmail
+     }
+
+     function insertEmail(member) {
+         return mailchimp.post('lists/' + mailchimpList + '/members/', member)
+     }
+ }
